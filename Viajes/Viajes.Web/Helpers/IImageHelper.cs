c12b2fa875e86aae19fc9f4ae9d94741edf1c4a7
JULIAN ANDRES OROZCO GARCIA <julianorozco236333@correo.itm.edu.co>
2020-04-02 21:58:56 -0500

    ADD Api for the user and add lenguages

---
+++ Viajes/Viajes.Web/Helpers/IImageHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Viajes.Web.Helpers
+{
+    public interface IImageHelper
+    {
+        string UploadImage(byte[] pictureArray, string folder);
+
+    }
+}
---
+++ Viajes/Viajes.Web/Helpers/ImageHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Viajes.Web.Helpers
+{
+    public class ImageHelper : IImageHelper
+    {
+        public string UploadImage(byte[] pictureArray, string folder)
+        {
+            MemoryStream stream = new MemoryStream(pictureArray);
+            string guid = Guid.NewGuid().ToString();
+            string file = $"{guid}.jpg";
+
+            try
+            {
+                stream.Position = 0;
+                string path = Path.Combine(Directory.GetCurrentDirectory(), $"wwwroot\\images\\{folder}", file);
+                File.WriteAllBytes(path, stream.ToArray());
+            }
+            catch
+            {
+                return string.Empty;
+            }
+
+            return $"~/images/{folder}/{file}";
+        }
+
+
+    }
+}
